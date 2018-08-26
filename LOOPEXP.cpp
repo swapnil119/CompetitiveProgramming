073f618e80f5916c6717ce1eb76ecafb7b22d0f0
swapnil119 <swapnilgupta119@gmail.com>
2018-08-26 17:13:49 +0530

    LOOPEXP solution added

--- LOOPEXP.cpp
+++ LOOPEXP.cpp
@@ -36,10 +36,25 @@ void __f(const char* names, Arg1&& arg1, Args&&... args){
 #else
 #define trace(...)
 #endif
+/*Linearity of Expectation.
+Count when each number will occur as max. in loop.
+for element x, it will be summation ((n-x)! * (x-1)! * f(x) )/ (n!)
+f(x) is placing x-1 elements if elements from x+1 to n occur after x. so now there are n-x+2 places and x-1 elements to fill. This can be done using integral solution. x1+x2+x3+..+xr=k, no. of ways = ncr(k+r-1,r-1)
+f(x)=ncr(n,i-1)
+Hence answer for x is 1/(n-x+1)*/
 int main()
 {
   std::ios::sync_with_stdio(false);
   cin.tie(NULL) ; cout.tie(NULL) ;
-  
+  int t,n;
+  cin>>t;
+  while(t--)
+    {
+      int n;
+      cin>>n;
+      db ans=0.0;
+      rep(i,1,n+1) ans+=1.0/(db)(n-i+1);
+      cout<<setprecision(10)<<fixed<<ans<<endl;
+    }
   return 0 ;
 }
