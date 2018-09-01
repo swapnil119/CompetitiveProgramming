9cc832fdeaf32e7c0fc7fe14e9b7acdad3c885a7
swapnil119 <swapnilgupta119@gmail.com>
2018-09-02 03:03:43 +0530

    Bookcase solution added

--- CompetitiveProgramming/UVA/UVA12099.cpp
+++ CompetitiveProgramming/UVA/UVA12099.cpp
@@ -36,10 +36,56 @@ void __f(const char* names, Arg1&& arg1, Args&&... args){
 #else
 #define trace(...)
 #endif
+const int N=75;
+ii a[N];
+/*index, thickness of 2nd shelf, thickness of 3rd shelf*/
+int dp[2][N*30][N*30];
 int main()
 {
   std::ios::sync_with_stdio(false);
   cin.tie(NULL) ; cout.tie(NULL) ;
+  int t;
+  cin>>t;
+  while(t--)
+    {
+      int n;
+      cin>>n;
+      rep(i,1,n+1) cin>>a[i].fi>>a[i].se;
+      sort(a+1,a+n+1); reverse(a+1,a+n+1);
+      rep(i,0,N*30) rep(j,0,N*30) dp[0][i][j]=dp[1][i][j]=1e9;
+      dp[1][0][0]=a[1].fi;
+      int sum=0,ans=1e9;
+      rep(i,2,n+1)
+	{
+	  int bit=i&1;
+	  rep(j,0,sum+1)
+	    {
+	      rep(k,0,sum-j+1)
+		{
+		  //give to first
+		  dp[bit][j][k]=min(dp[bit][j][k],dp[1-bit][j][k]);
+		  //give to second
+		  if(j==0) dp[bit][j+a[i].se][k]=min(dp[bit][j+a[i].se][k],dp[1-bit][j][k]+a[i].fi);
+		  else dp[bit][j+a[i].se][k]=min(dp[bit][j+a[i].se][k],dp[1-bit][j][k]);
+		  //give to third
+		  if(k==0)
+		    dp[bit][j][k+a[i].se]=min(dp[bit][j][k+a[i].se],dp[1-bit][j][k]+a[i].fi);
+		  else dp[bit][j][k+a[i].se]=min(dp[bit][j][k+a[i].se],dp[1-bit][j][k]);
+		}
+	    }
+	  rep(j,0,sum+1)
+	    rep(k,0,sum+1) dp[1-bit][j][k]=1e9;
+	  sum+=a[i].se;
+	}
+      rep(i,1,sum+1)
+	rep(j,1,sum-i+1)
+	{
+	  int mx=max(max(i,j),sum+a[1].se-i-j);
+	  if(dp[n&1][i][j]==1e9) continue;
+	  ans=min(ans,mx*dp[n&1][i][j]);
+	}
+      cout<<ans<<endl;
+    }
   
   return 0 ;
 }
