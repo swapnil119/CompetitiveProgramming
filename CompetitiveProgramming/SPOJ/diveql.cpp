bd16dc4166193d5860cce6ebf8f4c2ff036c7e8d
swapnil119 <swapnilgupta119@gmail.com>
2018-09-23 06:22:48 +0530

    diveql soln added

---
+++ CompetitiveProgramming/SPOJ/diveql.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define db long double
+#define ii pair<int,int>
+#define vi vector<int>
+#define fi first
+#define se second
+#define sz(a) (int)(a).size()
+#define all(a) (a).begin(),(a).end()
+#define pb push_back
+#define mp make_pair
+#define FN(i, n) for (int i = 0; i < (int)(n); ++i)
+#define FEN(i,n) for (int i = 1;i <= (int)(n); ++i)
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define repv(i,a,b) for(int i=b-1;i>=a;i--)
+#define SET(A, val) memset(A, val, sizeof(A))
+typedef tree<int ,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set ;
+// order_of_key (val): returns the no. of values less than val
+// find_by_order (k): returns the kth largest element.(0-based)
+#define TRACE
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+  cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+  const char* comma = strchr(names + 1, ','); cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+const int mod=1e9+7;
+int add(int x,int y)
+{
+  x+=y;
+  if(x>=mod) x-=mod;
+  if(x<0) x+=mod;
+  return x;
+}
+int mult(int x,int y)
+{
+  ll tmp=(ll)x*y;
+  if(tmp>=mod) tmp%=mod;
+  return tmp;
+}
+int pow1(int x,ll y)
+{
+  int ans=1;
+  while(y>0)
+    {
+      if(y%2==1) ans=mult(ans,x);
+      x=mult(x,x);
+      y/=2;
+    }
+  return ans;
+}
+//z*p^(n-1) * (p-1) = x*(p^n-1)
+//gcd will be p-1
+int main()
+{
+  std::ios::sync_with_stdio(false);
+  cin.tie(NULL) ;
+  int t;
+  cin>>t;
+  while(t--)
+    {
+      ll n; int p;
+      cin>>n>>p;
+      int x=pow1(p,n-1);
+      int z=mult(add(pow1(p,n),-1),pow1(p-1,mod-2));
+      cout<<z<<" "<<x<<'\n';
+    }
+  return 0 ;
+}
